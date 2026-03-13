944056831662a700e0e3c5daa3a416a0b1870aa4
Nick Panaccione <nicholaspanaccione@gmail.com>
2026-03-13 11:45:41 -0700

    update core code styling

--- source/core/PluginEditor.h
+++ source/core/PluginEditor.h
@@ -1,33 +1,17 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
-
 #pragma once
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-//==============================================================================
-/**
-*/
-class DrawFormAudioProcessorEditor  : public juce::AudioProcessorEditor
-{
+class DrawFormAudioProcessorEditor  : public juce::AudioProcessorEditor {
 public:
-    DrawFormAudioProcessorEditor (DrawFormAudioProcessor&);
-    ~DrawFormAudioProcessorEditor() override;
+  DrawFormAudioProcessorEditor (DrawFormAudioProcessor&);
+  ~DrawFormAudioProcessorEditor() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
+  void paint (juce::Graphics&) override;
+  void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    DrawFormAudioProcessor& audioProcessor;
-
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DrawFormAudioProcessorEditor)
+  DrawFormAudioProcessor& audioProcessor;
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DrawFormAudioProcessorEditor)
 };
